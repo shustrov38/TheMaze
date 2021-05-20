81c056460e451642a90964a044c71a36209a05a0
shustrov38 <shustrov38@gmail.com>
2021-05-20 18:37:48 +0300

    Inital commit

---
+++ src/utilities.h
@@ -0,0 +1,14 @@
+#ifndef THEMAZE_UTILITIES_H
+#define THEMAZE_UTILITIES_H
+
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <string.h>
+
+#include <windows.h>
+
+#include "ok_lib.h"
+
+#endif //THEMAZE_UTILITIES_H
