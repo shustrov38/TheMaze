22fcd17215c98934961ed18e1f7254daf3553c1d
akon1te <anros_t@mail.ru>
2021-06-03 18:07:28 +0300

    added players struct (but pls use only 1 player)

--- src/local-game/game.c
+++ src/local-game/game.c
@@ -4,83 +4,79 @@
 #include "playerInfo.h"
 
 
+playerPos *initAllPlayers(int playersCnt) {
+    playerPos *players = (playerPos*) malloc (playersCnt * sizeof(playerPos));
+
+    for (int i = 0; i < playersCnt; ++i) {
+        if (i == 0) {
+            players[i].X = 1;
+            players[i].Y = 1;
+            players[i].Prev_X = 1;
+            players[i].Prev_Y = 1;
+        } else if (i == 1){
+            players[i].X = MAZE_SIZE - 2;
+            players[i].Y = MAZE_SIZE - 2;
+            players[i].Prev_X = MAZE_SIZE - 2;
+            players[i].Prev_Y = MAZE_SIZE - 2;
+        } else if (i == 2){
+            players[i].X = MAZE_SIZE - 2;
+            players[i].Y = 1;
+            players[i].Prev_X = MAZE_SIZE - 2;
+            players[i].Prev_Y = 1;
+        } else if (i == 3){
+            players[i].X = 1;
+            players[i].Y = MAZE_SIZE - 2;
+            players[i].Prev_X = 1;
+            players[i].Prev_Y = MAZE_SIZE - 2;
+        }
+    }
+
+    return players;
+}
+
 int TILE_SIZE = 15;
 int SCREEN_SIZE = 750;
 
-playerPos player;
-
-playerPos initPlayer() {
-    player.X = 1;
-    player.Y = 1;
-    return player;
-}
 
 static int game_status;
 static Uint32 moveEventTime,
-        lvlEventTime;
+              lvlEventTime;
 
 
-static void Process_events(int **maze) {
+static void Process_events() {
     SDL_Event event;
-    SDL_Keycode keycode;
-    Uint32 curTime = SDL_GetTicks();
-    const Uint8 *curKey = SDL_GetKeyboardState(NULL);
 
     while (SDL_PollEvent(&event) != 0) {
-        keycode = event.key.keysym.sym;
-
         if (event.type == SDL_QUIT) {
             game_status = GAME_OVER;
             return;
         }
     }
+}
 
-    //move of player
-    if ((GAME_RUNNING == game_status) && (curTime - moveEventTime >= 20)) {
-        moveEventTime = curTime;
-
-        if (curKey[SDL_SCANCODE_UP] || curKey[SDL_SCANCODE_W]) {
-            if (maze[player.X][player.Y - 1] != 1) {
-                player.Prev_Y = player.Y;
-                player.Y -= 1;
-            }
-        }
-        if (curKey[SDL_SCANCODE_LEFT] || curKey[SDL_SCANCODE_A]) {
-            if (maze[player.X - 1][player.Y] != 1) {
-                player.Prev_X = player.X;
-                player.X -= 1;
-            }
-        }
-        if (curKey[SDL_SCANCODE_RIGHT] || curKey[SDL_SCANCODE_D]) {
-            if (maze[player.X + 1][player.Y] != 1) {
-                player.Prev_X = player.X;
-                player.X += 1;
-            }
-        }
-        if (curKey[SDL_SCANCODE_DOWN] || curKey[SDL_SCANCODE_S]) {
-            if (maze[player.X][player.Y + 1] != 1) {
-                player.Prev_Y = player.Y;
-                player.Y += 1;
-            }
-        }
+int checkFinishPoint(playerPos player, int **maze) {
+    if (maze[player.X][player.Y] == 5) {
+        return GAME_OVER;
+    } else {
+        return GAME_RUNNING;
     }
 }
-int checkFinishPoint(int **maze){
-   if (maze[player.X][player.Y] == 5) {
-       return GAME_OVER;
-   } else {
-       return GAME_RUNNING;
-   }
-}
+
 
 int WinMain(int argc, char *argv[]) {
     srand(time(NULL));
 
+    int playersCnt;
+    printf("Enter players count: ");
+    scanf("%d", &playersCnt);
 
+    playerPos *players = initAllPlayers(playersCnt);
+
+//textures
     //SDL_Surface *background = Load_img("D:\\AllCodes\\TheMaze\\src\\local-game\\Textures\\wall2.jpg");
     //SDL_Surface *scaled_background = ScaleSurface(background,750,750);
 
-    SDL_Surface *wall = Load_img("D:\\AllCodes\\TheMaze\\src\\local-game\\Textures\\wall.jpg");
+    SDL_Surface *wall = Load_img("../../../src/local-game/Textures/wall.jpg");
     SDL_Surface *scaled_wall = ScaleSurface(wall, TILE_SIZE, TILE_SIZE);
 
     SDL_Surface *black = Load_img("D:\\AllCodes\\TheMaze\\src\\local-game\\Textures\\black.bmp");
@@ -93,9 +89,7 @@ int WinMain(int argc, char *argv[]) {
     SDL_Surface *playerIcon = Load_img(
             "D:\\AllCodes\\TheMaze\\src\\local-game\\Textures\\Phantom_Assassin_minimap_icon.png");
     SDL_Surface *scaled_playerIcon = ScaleSurface(playerIcon, TILE_SIZE, TILE_SIZE);
-
-
-    initPlayer();
+//textures
 
     moveEventTime = SDL_GetTicks();
     lvlEventTime = moveEventTime;
@@ -106,15 +100,13 @@ int WinMain(int argc, char *argv[]) {
     int width = MAZE_SIZE;
     int **maze;
 
+    //maze generation
     maze = initMaze(maze, width, height);
     maze = generator(maze, 0, width - 1, 0, height - 1);
-
     maze = createFinishPoint(maze, width, height);
-
     Init_window("Maze", SCREEN_SIZE, SCREEN_SIZE);
 
-    Draw_image(screen, scaled_playerIcon, player.X * TILE_SIZE, player.Y * TILE_SIZE);
-    Update_window_rect(0, 0, SCREEN_SIZE, SCREEN_SIZE);
+
 
     if (GAME_RUNNING == game_status) {
 
@@ -133,15 +125,23 @@ int WinMain(int argc, char *argv[]) {
             }
         }
 
+        //begin positions
+        for (int i = 0; i < playersCnt; ++i) {
+            Draw_image(screen, scaled_playerIcon, players[i].X * TILE_SIZE, players[i].Y * TILE_SIZE);
+        }
         Update_window_rect(0, 0, SCREEN_SIZE, SCREEN_SIZE);
     }
 
+
     while (game_status != GAME_OVER){
-        Process_events(maze);
-        game_status = checkFinishPoint(maze);
-        Draw_image(screen, scaled_playerIcon, TILE_SIZE * player.X, TILE_SIZE * player.Y);
+        Process_events();
+        players[0] = playerMoves(maze, players[0]);
+        game_status = checkFinishPoint(players[0], maze);
+        Draw_image(screen, scaled_black, TILE_SIZE * players[0].Prev_X, TILE_SIZE * players[0].Prev_Y);
+        Draw_image(screen, scaled_playerIcon, TILE_SIZE * players[0].X, TILE_SIZE * players[0].Y);
         Update_window_rect(0, 0, SCREEN_SIZE, SCREEN_SIZE);
-        SDL_Delay(150);
+
+        SDL_Delay(125);
     }
 
 }
--- src/local-game/game.h
+++ src/local-game/game.h
@@ -2,12 +2,7 @@
 #define THEMAZE_GAME_H
 
 
-typedef struct {
-    int X;
-    int Y;
-    int Prev_X;
-    int Prev_Y;
-} playerPos;
+
 
 
 #endif //THEMAZE_GAME_H
