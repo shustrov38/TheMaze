c67801abd414747916a031b3fe40a455a5b68ff4
shustrov38 <shustrov38@gmail.com>
2021-06-04 02:45:45 +0300

    server: created pass func. client: refactor

--- src/client/client.c
+++ src/client/client.c
@@ -2,19 +2,104 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-int hash (char *s1, char *s2){
-    int base = 23;
-    int res = 0;
-    for(int i = 0; i < min(strlen(s1),strlen(s2)); i++){
-        res+= (s1[i] + s2[i]) * base;
-        base *= base;
+SOCKET client;
+
+typedef enum {
+    //TAG:          // WHAT IT DOES:                     ARGS "<> ...":            DIR:
+    CONNECTION,     // connect this client to server    "<CONNECTION> NAME PASS"   TO
+    SYS_MSG,        // message STR from/to server       "<SYS_MSG> STR"            TO/FROM
+    INVITE,         // invite NAME/invite from NAME     "<INVITE> NAME"            TO/FROM
+    MOV_RIVAL,      // move enemy NAME to X Y           "<MOV_RIVAL> NAME X Y"     FROM
+    MOV_SELF        // move this player to X Y          "<MOV_SELF> X Y"           TO
+} INNER_INTERFACE;
+
+typedef struct {
+    INNER_INTERFACE TAG;
+    char ARG_0[16];
+    char ARG_1[16];
+    char ARG_2[16];
+    char ARG_3[16];
+    char ARG_4[16];
+} COMMAND_PROTOTYPE;
+
+void make_command(COMMAND_PROTOTYPE proto) {
+    char command[1024];
+    memset(command, 0, 1024);
+    switch (proto.TAG) {
+        case CONNECTION:
+            sprintf(command, "%s %s %s", "<CONNECTION>", proto.ARG_0, proto.ARG_1);
+            break;
+        case SYS_MSG:
+            sprintf(command, "%s %s", "<SYS_MSG>", proto.ARG_0);
+            break;
+        case MOV_SELF:
+            sprintf(command, "%s %s %s", "<MOV_SELF>", proto.ARG_0, proto.ARG_1);
+            break;
     }
-    return res % (int)10e6;
+    printf("%s\n", command);
+    send(client, command, 1024, 0);
 }
 
-void SendData2Server(char *name) {
-    SOCKET client;
+COMMAND_PROTOTYPE C;
+
+#define LOGIN(NAME, PASS)    C.TAG = CONNECTION;\
+                            strcpy(C.ARG_0,NAME);\
+                            strcpy(C.ARG_1,PASS);\
+                            make_command(C)\
+
+#define SAY(MSG)            C.TAG = SYS_MSG ;\
+                            strcpy(C.ARG_0,MSG);\
+                            make_command(C)\
+
+#define GO(X, Y)             C.TAG = MOV_SELF;\
+                            strcpy(C.ARG_0,itoa(X,C.ARG_0,10));\
+                            strcpy(C.ARG_1,itoa(Y,C.ARG_1,10));\
+                            make_command(C)\
+
+
+//int stopIssued = 1;
+//pthread_mutex_t stopMutex;
+//
+//int getStopIssued() {
+//    int ret = 0;
+//    pthread_mutex_lock(&stopMutex);
+//    ret = stopIssued;
+//    pthread_mutex_unlock(&stopMutex);
+//    return ret;
+//}
+//
+//void setStopIssued(int val) {
+//    pthread_mutex_lock(&stopMutex);
+//    stopIssued = val;
+//    pthread_mutex_unlock(&stopMutex);
+//}
+//
+//void *command_callback(void *param) {
+//    SOCKET server = (SOCKET) param;
+//    int status;
+//
+//    while (getStopIssued()) {
+//
+//
+//        if(strlen(command)==0){
+//            continue;
+//        }
+//
+//        status = send(server, command, 128, 0);
+//        memset(command,0,128);
+//
+//        if(status==SOCKET_ERROR){
+//            printf("[CLIENT ERROR] An error occurred after trying to send command");
+//            setStopIssued(0);
+//        }
+//
+//    }
+//    return (void *) 0;
+//}
+
+void startSession() {
     client = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
     if (client == INVALID_SOCKET) {
         printf("Error create socket\n");
@@ -23,68 +108,67 @@ void SendData2Server(char *name) {
     struct sockaddr_in server;
     server.sin_family = AF_INET;
     server.sin_port = htons(5510); //the same as in server
-    server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); //special look-up address
-//    server.sin_addr.S_un.S_addr = inet_addr("26.173.251.89");
+//  server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1"); //special look-up address
+    server.sin_addr.S_un.S_addr = inet_addr("26.173.251.89");
     if (connect(client, (struct sockaddr *) &server, sizeof(server)) == SOCKET_ERROR) {
         printf("Can't connect to server\n");
         closesocket(client);
         return;
     }
 
-    char new_msg[128];
 
-    char *pass = malloc(sizeof(char) * 32);
-    memset(pass, 0, 32);
-    strcat(pass, "qwerty");
-    strcat(pass, name);
+    char name[32];
+    char pass[32];
 
-    sprintf(new_msg, "<new> %s %s", name, pass);
+    printf("Welcome to the Maze!\n");
+    printf("Nickname:");
+    scanf("%s", name);
+    printf("Password:");
+    scanf("%s", pass);
 
-    int ret = send(client, new_msg, 128, 0);
-    if (ret == SOCKET_ERROR) {
-        printf("Can't send message\n");
-        closesocket(client);
-        return;
-    }
+//    setStopIssued(1);
+//
+//    int com_status;
+//    pthread_t command_thread;
+//    com_status = pthread_create(&command_thread, NULL, command_callback, (void *) client);
+//    com_status = pthread_detach(command_thread);
 
-    for (int i = 0; i < 10000; ++i) {
-        char message[1024];
-        sprintf(message, "%s %d", "test", i);
-        int ret = send(client, message, (int) strlen(message), 0);
-        if (ret == SOCKET_ERROR) {
-            printf("Can't send message\n");
-            closesocket(client);
-            return;
-        }
+//    int rec_status;
+//    pthread_t rec_thread;
+//    rec_status = pthread_create(&command_thread, NULL, command_callback, (void *) client);
+//    rec_status = pthread_detach(command_thread);
 
-        printf("Sent: %s\nbytes: %d\n\n", message, ret);
-        ret = SOCKET_ERROR;
-        while (ret == SOCKET_ERROR) {
-            //полчение ответа
-            ret = recv(client, message, 1024, 0);
-            //обработка ошибок
-            if (ret == 0 || ret == WSAECONNRESET) {
-                printf("Connection closed\n");
-                break;
-            }
-            if (ret < 0) {
-                printf("Can't receive message\n");
-                closesocket(client);
-                return;
-            }
-            //вывод на экран количества полученных байт и сообщение
-            system("cls");
-            printf("Receive: \n%s\n bytes: %d\n", message, ret);
-        }
-    }
-
-    sprintf(new_msg, "<end>");
-    ret = send(client, new_msg, 5, 0);
-    if (ret == SOCKET_ERROR) {
-        printf("Can't send message\n");
-        closesocket(client);
-        return;
-    }
+    LOGIN(name, pass);
+    //    for (int i = 0; i < 10000; ++i) {
+//        char message[1024];
+//        sprintf(message, "%s %d", "test", i);
+//        int ret = send(client, message, (int) strlen(message), 0);
+//        if (ret == SOCKET_ERROR) {
+//            printf("Can't send message\n");
+//            closesocket(client);
+//            return;
+//        }
+//
+//        printf("Sent: %s\nbytes: %d\n\n", message, ret);
+//        ret = SOCKET_ERROR;
+//        while (ret == SOCKET_ERROR) {
+//            //полчение ответа
+//            ret = recv(client, message, 1024, 0);
+//            //обработка ошибок
+//            if (ret == 0 || ret == WSAECONNRESET) {
+//                printf("Connection closed\n");
+//                break;
+//            }
+//            if (ret < 0) {
+//                printf("Can't receive message\n");
+//                closesocket(client);
+//                return;
+//            }
+//            //вывод на экран количества полученных байт и сообщение
+//            system("cls");
+//            printf("Receive: \n%s\n bytes: %d\n", message, ret);
+//        }
+//    }
 
     closesocket(client);
 }
@@ -95,13 +179,12 @@ int main() {
         printf("Can't connect to socket lib");
         return 1;
     }
-    int i = 0;
     srand(time(0));
     rand();
     int number = abs(rand()) % 1000;
     char *name = malloc(sizeof(char) * 32);
-    sprintf(name, "%d",number+1488);
-    SendData2Server(name);
+    sprintf(name, "%d", number + 1488);
+    startSession();
     printf("Session is closed\n");
     Sleep(1000);
     return 0;
--- src/server/server.c
+++ src/server/server.c
@@ -12,6 +12,14 @@
 
 #define SQL_ERROR 10
 
+char *get_current_time() {
+    time_t r_time;
+    time(&r_time);
+    char *buffer = asctime(localtime(&r_time));
+    buffer[strlen(buffer) - 1] = 0;
+    return buffer;
+}
+
 int hash(char *s1, char *s2) {
     int base = 23;
     int res = 0;
@@ -32,8 +40,17 @@ void pthread_fprintf(char *format, ...) {
     va_start(arg, format);
     pthread_mutex_lock(&mutex_file);
     vprintf(format, arg);
+    fflush(stdout);
     pthread_mutex_unlock(&mutex_file);
     va_end(arg);
+}
+
+void printf_server_error_prefix() {
+    pthread_fprintf("[SERVER ERROR][%s] ", get_current_time());
+}
+
+void printf_server_prefix() {
+    pthread_fprintf("[SERVER][%s] ", get_current_time());
 }
 
 int stopIssued = 1;
@@ -68,6 +85,7 @@ int db_execute(char *sql, int (*callback)(void *, int, char **, char **), void *
     return SUCCESS;
 }
 
+
 // sql thread safe execution
 #define SQL_THREAD_EXEC(sql, callback, arg, err) if (db_execute(sql, callback, arg, err) != SUCCESS) return (void *) SQL_ERROR
 
@@ -79,17 +97,28 @@ int callback_check_if_exists(void *param, int argc, char **argv, char **col_name
     return 0;
 }
 
-char *get_current_time() {
-    time_t r_time;
-    time(&r_time);
-    char *buffer = asctime(localtime(&r_time));
-    buffer[strlen(buffer) - 1] = 0;
-    return buffer;
+int callback_select_all_online_users(void *param, int argc, char **argv, char **col_name) {
+    char *online_users = (char *) param;
+    for (int i = 0; i < argc; ++i) {
+        strcat(online_users, argv[i]);
+        strcat(online_users, "\n");
+    }
+    return 0;
+}
+
+int callback_get_one_string(void *param, int argc, char **argv, char **col_name) {
+    char *result = (char *) param;
+    for (int i = 0; i < argc; ++i) {
+        sprintf(result, "%s", argv[i]);
+    }
+    return 0;
 }
 
 void *client_callback(void *param) {
     SOCKET client = (SOCKET) param;
     char *err;
+
+    int first_time = 1;
 
     char tag[64];
     char login[64], password[64];
@@ -97,7 +126,9 @@ void *client_callback(void *param) {
     char string_id[10] = "";
     char sql_update_online_0[128];
     char sql_update_online_1[128];
-    char sql_if_exists[128];
+    char sql_if_exists_name[128];
+    char sql_select_all_online_users[128] = "SELECT Login FROM Data WHERE Online = 1;";
+    char sql_get_password_by_name[128];
 
     while (1) {
         char receive[1024], transmit[1024];
@@ -105,34 +136,41 @@ void *client_callback(void *param) {
 
         ret = recv(client, receive, 1024, 0);
         if (!ret || ret == SOCKET_ERROR) {
-            pthread_fprintf("[SERVER ERROR] Can't receive data from the client {login: \"%s\"}.\n", login);
+            printf_server_error_prefix();
+            pthread_fprintf("Can't receive data from the client {login: \"%s\"}.\n", login);
             SQL_THREAD_EXEC(sql_update_online_0, 0, 0, err);
-            pthread_fprintf("[SERVER][%s] Client {login: \"%s\"} disconnected.\n", get_current_time(), login);
+            printf_server_prefix();
+            pthread_fprintf("Client {login: \"%s\"} disconnected.\n", login);
             return (void *) RECEIVE_ERROR;
         }
 
         receive[ret] = '\0';
         sscanf(receive, "%s", tag);
 
-        if (!strcmp(tag, "<end>")) {
+        if (!strcmp(tag, "<EXIT>")) {
             SQL_THREAD_EXEC(sql_update_online_0, 0, 0, err);
-            pthread_fprintf("[SERVER][%s] Client {login: \"%s\"} disconnected.\n", get_current_time(), login);
+            printf_server_prefix();
+            pthread_fprintf("Client {login: \"%s\"} disconnected.\n", login);
             break;
         }
 
-        if (!strcmp(tag, "<new>")) {
+        if (!strcmp(tag, "<CONNECTION>") && first_time) {
+            first_time = 0;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             sscanf(receive, "%s %s %s", tag, login, password);
 
-            pthread_fprintf("[SERVER][%s] Client {login: \"%s\", password: \"%s\"}.\n",
-                            get_current_time(), login, password);
+            printf_server_prefix();
+            pthread_fprintf("Client {login: \"%s\", password: \"%s\"} connected.\n", login, password);
+
             itoa(hash(login, password), string_id, 10);
+
             sprintf(sql_update_online_0, "UPDATE Data SET Online = 0 WHERE Id = %s", string_id);
             sprintf(sql_update_online_1, "UPDATE Data SET Online = 1 WHERE Id = %s", string_id);
-            sprintf(sql_if_exists, "SELECT EXISTS(SELECT Id  FROM Data WHERE Id = %s LIMIT 1) AS exist;", string_id);
+            sprintf(sql_if_exists_name, "SELECT EXISTS(SELECT Id FROM Data WHERE Login = \'%s\' LIMIT 1) AS exist;", login);
+            sprintf(sql_get_password_by_name, "SELECT Password FROM Data WHERE Login = \'%s\';", login);
 
             int exists = 0;
-            SQL_THREAD_EXEC(sql_if_exists, callback_check_if_exists, (void *) &exists, err);
+            SQL_THREAD_EXEC(sql_if_exists_name, callback_check_if_exists, (void *) &exists, err);
 
             if (!exists) {
                 char sql_add_client[128];
@@ -140,50 +178,35 @@ void *client_callback(void *param) {
 
                 SQL_THREAD_EXEC(sql_add_client, 0, 0, err);
             } else {
+                char stored_password[64];
+                SQL_THREAD_EXEC(sql_get_password_by_name, callback_get_one_string, (void *) stored_password, err);
+                if (strcmp(stored_password, password) != 0) {
+                    printf_server_prefix();
+                    pthread_fprintf("Client {login: \"%s\", password: \"%s\"} <- in danger.\n", login, stored_password);
+                    return (void *) 1;
+                }
                 SQL_THREAD_EXEC(sql_update_online_1, 0, 0, err);
             }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
         }
 
-//        sprintf(transmit, "%s %s %s\n", "Your data", receive, "received");
+        printf_server_prefix();
+        pthread_fprintf("Client {login: \"%s\", password: \"%s\"}: %s.\n", login, password, (strlen(receive) ? receive : "EMPTY MESSAGE"));
 
         memset(transmit, 0, 1024);
-        while (sqlite3_mutex_try(db_mutex) == SQLITE_BUSY);
-        char sql_get_online[128] = "SELECT Login FROM Data WHERE Online = 1;";
-        sqlite3_stmt *stmt = NULL;
-        int retval = sqlite3_prepare_v2(db, sql_get_online, -1, &stmt, 0);
-        if (retval) {
-            pthread_fprintf("Selecting data from DB Failed (err_code=%d)\n", retval);
-            return (void *) SQL_ERROR;
-        }
-        int idx = 0;
-        while (1) {
-            retval = sqlite3_step(stmt);
-
-            if (retval == SQLITE_ROW) {
-                char *val = (char *) sqlite3_column_text(stmt, 0);
-                strcat(transmit, val);
-                strcat(transmit, "\n");
-            } else if (retval == SQLITE_DONE) {
-                break;
-            } else {
-                sqlite3_finalize(stmt);
-                pthread_fprintf("Some error encountered\n");
-                break;
-            }
-        }
-        sqlite3_finalize(stmt);
-        sqlite3_mutex_leave(db_mutex);
+        SQL_THREAD_EXEC(sql_select_all_online_users, callback_check_if_exists, (void *) transmit, err);
 
         Sleep(500);
 
-        ret = send(client, transmit, sizeof(transmit), 0);
-        if (ret == SOCKET_ERROR) {
-            pthread_fprintf("[SERVER ERROR] Can't send data to the client {login: \"%s\"}.\n", login);
-            SQL_THREAD_EXEC(sql_update_online_0, 0, 0, err);
-            pthread_fprintf("[SERVER][%s] Client {login: \"%s\"} disconnected.\n", get_current_time(), login);
-            return (void *) SEND_ERROR;
-        }
+//        ret = send(client, transmit, sizeof(transmit), 0);
+//        if (ret == SOCKET_ERROR) {
+//            printf_server_error_prefix();
+//            pthread_fprintf("Can't send data to the client {login: \"%s\"}.\n", login);
+//            SQL_THREAD_EXEC(sql_update_online_0, 0, 0, err);
+//            printf_server_prefix();
+//            pthread_fprintf("Client {login: \"%s\"} disconnected.\n", login);
+//            return (void *) SEND_ERROR;
+//        }
     }
 
     return (void *) SUCCESS;
